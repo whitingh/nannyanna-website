bf9c8148126b1b208d9afaa4b0d1d9ee2b9a53f8
Harry Whiting <whitingh32@gmail.com>
2026-08-07 22:35:03 +0100

    Create Next.js website

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>NannyAnna</h1>
+      <p>
+        Dudle is the swaggiest swagmeister in all of swagland
+      </p>
+    </main>
+  );
+}
